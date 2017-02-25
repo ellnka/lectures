15ceda5a17dee30832173c5a1adfe6b367bd6ab2
Kirill Blyumkin <kirill.blyumkin@kohls.com>
2017-02-25 14:06:42 +0200

    .gitignore added

---
+++ src/edu/source/it/lectures/lecture1/PrimitivesChecks.java
@@ -0,0 +1,25 @@
+package edu.source.it.lectures.lecture1;
+
+public class PrimitivesChecks {
+    public static void main(String[] args) {
+        /*int x = 100;
+        long l = 12312345678987654L;
+
+        System.out.println(x % 6);
+
+        //float f = (float)123.1;
+        float f = 123.1f;
+        double d = 123.1;
+        */
+        boolean b = true;
+        boolean b1 = false;
+
+        System.out.println(b & b1);
+        System.out.println(b && b1);
+        System.out.println(b | b1);
+        System.out.println(b || b1);
+
+
+
+    }
+}
